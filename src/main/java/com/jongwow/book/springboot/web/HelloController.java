d83e090080f708ac12ed1fb949ea83ba54d48d48
JongWoo Park <jjongwoo.dev@gmail.com>
2020-06-04 23:24:57 +0900

    chapter 2.4 lombok으로 전환 완료 및 테스트 코드 작성

--- src/main/java/com/jongwow/book/springboot/web/HelloController.java
+++ src/main/java/com/jongwow/book/springboot/web/HelloController.java
@@ -1,12 +1,22 @@
 package com.jongwow.book.springboot.web;
+
+import com.jongwow.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        // @RequestParam: 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션. 여기선 name (@Req~) 이란 이름으로 넘긴 파라미터를
+        // 메소드 파라미터 name(String name)에 저장함.
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/jongwow/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jongwow/book/springboot/web/HelloControllerTest.java
@@ -8,9 +8,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -27,4 +28,25 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+        /*
+         * Param
+         * 0 API 테스트할 때의 요청 파라미터 설정
+         * 단, 값은 String
+         * 그래서 숫자/날짜 등도 문자열로 변경
+         *
+         * jsonPath
+         * JSON응답값을 필드별로 검증할 수 있는 메소드
+         * $를 기준으로 필드명 명시
+         **/
+    }
 }
