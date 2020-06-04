7d6a02062d178ecb8e95d0c1ad7b89ff3b293165
JongWoo Park <jjongwoo.dev@gmail.com>
2020-06-05 02:13:38 +0900

    Chapter 3.4 API controller 등록 API 구현

---
+++ src/main/java/com/jongwow/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.jongwow.book.springboot.service.posts;
+
+import com.jongwow.book.springboot.domain.posts.PostsRepository;
+import com.jongwow.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
